4f13cf992a5aff6d4e32e8a1d2f069a8f18801fe
Varsha Meghana Kanmuri <vk2497@columbia.edu>
2023-11-12 20:27:09 -0500

    adding API Key and voice support

--- assets/js/index.js
+++ assets/js/index.js
@@ -157,3 +157,25 @@ function getSearch(searchText) {
         'q': searchText
     }, {}, {});
 }
+
+click_to_record.addEventListener('click',function(){
+    var record = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+       
+        document.getElementById("searchKey").value = transcript;
+    });
+    
+    if (record == true) {
+        recognition.start();
+    }
+})
